84fc7abf35e0ab97f5461ee37570fdf74986fc83
junsu <melon8372@gmail.com>
2020-09-12 22:31:01 +0900

    feature: Add detail_screen, onPressed event click

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test_app/model/model_movie.dart';
+import 'package:netflix_clone_test_app/screen/detail_screen.dart';
 
 class CarouselImage extends StatefulWidget {
   final List<Movie> movies;
@@ -106,7 +107,16 @@ class _CarouselImageState extends State<CarouselImage> {
                     children: <Widget>[
                       IconButton(
                         icon: Icon(Icons.info),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute<Null>(
+                            fullscreenDialog: true,
+                            builder: (BuildContext context) {
+                              return DetailScreen(
+                                movie: movies[_currentPage],
+                              );
+                            },
+                          ));
+                        },
                       ),
                       Text(
                         '정보',
--- lib/widget/circle_slider.dart
+++ lib/widget/circle_slider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test_app/model/model_movie.dart';
+import 'package:netflix_clone_test_app/screen/detail_screen.dart';
 
 class CircleSlider extends StatelessWidget {
   final List<Movie> movies;
@@ -18,7 +19,7 @@ class CircleSlider extends StatelessWidget {
             height: 120,
             child: ListView(
               scrollDirection: Axis.horizontal,
-              children: makeCircleImages(movies),
+              children: makeCircleImages(context, movies),
             ),
           ),
         ],
@@ -27,12 +28,22 @@ class CircleSlider extends StatelessWidget {
   }
 }
 
-List<Widget> makeCircleImages(List<Movie> movies) {
+List<Widget> makeCircleImages(BuildContext context, List<Movie> movies) {
   List<Widget> results = [];
   for (var i = 0; i < movies.length; i++) {
     results.add(
-      InkWell( // enable click
-        onTap: () {},
+      InkWell(
+        // enable click
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute<Null>(
+            fullscreenDialog: true,
+            builder: (BuildContext context) {
+              return DetailScreen(
+                movie: movies[i],
+              );
+            },
+          ));
+        },
         child: Container(
           padding: EdgeInsets.only(right: 10),
           child: Align(
