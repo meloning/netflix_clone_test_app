16cd65decab1f6a67548a07c41464fce214dc005
junsu <melon8372@gmail.com>
2020-09-12 23:46:38 +0900

    feature: Add more_screen based on profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test_app/screen/home_screen.dart';
+import 'package:netflix_clone_test_app/screen/more_screen.dart';
 import 'package:netflix_clone_test_app/widget/bottom_bar.dart';
 
 void main() {
@@ -39,11 +40,7 @@ class _MyAppState extends State<MyApp> {
                   child: Text('save'),
                 ),
               ),
-              Container(
-                child: Center(
-                  child: Text('more'),
-                ),
-              ),
+              MoreScreen(),
             ],
           ),
           bottomNavigationBar: Bottom(),
