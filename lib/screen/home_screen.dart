6556a60ab0289e2a210f22d8586e3e8c878d1a8a
junsu <melon8372@gmail.com>
2020-09-12 21:46:26 +0900

    feature: Add circle_slider, box_slider widget in main home

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test_app/model/model_movie.dart';
+import 'package:netflix_clone_test_app/widget/box_slider.dart';
 import 'package:netflix_clone_test_app/widget/carousel_slider.dart';
+import 'package:netflix_clone_test_app/widget/circle_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -51,6 +53,12 @@ class _HomeScreenState extends State<HomeScreen> {
             TopBar(),
           ],
         ),
+        CircleSlider(
+          movies: movies,
+        ),
+        BoxSlider(
+          movies: movies,
+        )
       ],
     );
   }
